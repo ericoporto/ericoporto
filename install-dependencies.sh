4490c002ff52d6da16a50bd59c7e1bd1c0571339
ericoporto <ericoporto2008@gmail.com>
2018-09-15 20:58:53 -0300

    adding new ericoporto.github.io page portifolio

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+gem install bundler jekyll
+gem install jekyll-sitemap
+gem install jekyll-paginate
+gem install jekyll-gist
+gem install jekyll-feed
+gem install jekyll-data
+gem install jemoji
